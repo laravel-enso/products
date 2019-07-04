bcc19eb39836960bd4e4de8c253c1dcacb5cb0fd
Adrian Ocneanu <aocneanu@gmail.com>
2019-07-04 12:19:42 +0000

    Apply fixes from StyleCI

--- src/app/Http/Requests/ValidateProductRequest.php
+++ src/app/Http/Requests/ValidateProductRequest.php
@@ -3,8 +3,6 @@
 namespace LaravelEnso\Products\app\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use LaravelEnso\Companies\app\Models\Company;
-use LaravelEnso\Products\app\Models\MeasurementUnit;
 
 class ValidateProductRequest extends FormRequest
 {
--- src/database/factories/ProductFactory.php
+++ src/database/factories/ProductFactory.php
@@ -15,7 +15,7 @@ $factory->define(Product::class, function (Faker $faker) {
         },
         'name' => $faker->word,
         'part_number' => Product::max('part_number') + 1,
-        'internal_code' => 'CT-' . $faker->numberBetween(0, 500),
+        'internal_code' => 'CT-'.$faker->numberBetween(0, 500),
         'list_price' => $faker->numberBetween(1, 300),
         'vat_percent' => collect([5, 19, 24])->random(),
         'package_quantity' => $faker->numberBetween(0, 5),
