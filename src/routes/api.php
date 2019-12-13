9bf520656a28447e168a0ef1228aabf4673750ca
Mihai <mihai.ocneanu@gmail.com>
2019-12-13 18:00:19 +0200

    split routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,21 +4,5 @@ Route::middleware(['web', 'auth', 'core'])
     ->namespace('LaravelEnso\Products\app\Http\Controllers')
     ->prefix('api')
     ->group(function () {
-        Route::namespace('Products')
-            ->prefix('products')
-            ->as('products.')
-            ->group(function () {
-                Route::get('create', 'Create')->name('create');
-                Route::post('', 'Store')->name('store');
-                Route::get('{product}/edit', 'Edit')->name('edit');
-                Route::patch('{product}', 'Update')->name('update');
-                Route::delete('{product}', 'Destroy')->name('destroy');
-
-                Route::get('initTable', 'InitTable')->name('initTable');
-                Route::get('tableData', 'TableData')->name('tableData');
-                Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
-
-                Route::get('options', 'Options')->name('options');
-                Route::get('suppliers', 'Suppliers')->name('suppliers');
-            });
+        require 'app/products.php';
     });
