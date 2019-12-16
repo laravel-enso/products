76d4b9e6a92ea4e85e5c53df4e4059096e3f84fa
Mihai <mihai.ocneanu@gmail.com>
2019-12-16 09:20:30 +0200

    undid route files modification

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,5 +4,21 @@ Route::middleware(['web', 'auth', 'core'])
     ->namespace('LaravelEnso\Products\app\Http\Controllers')
     ->prefix('api')
     ->group(function () {
-        require 'app/products.php';
+        Route::namespace('Products')
+            ->prefix('products')
+            ->as('products.')
+            ->group(function () {
+                Route::get('create', 'Create')->name('create');
+                Route::post('', 'Store')->name('store');
+                Route::get('{product}/edit', 'Edit')->name('edit');
+                Route::patch('{product}', 'Update')->name('update');
+                Route::delete('{product}', 'Destroy')->name('destroy');
+
+                Route::get('initTable', 'InitTable')->name('initTable');
+                Route::get('tableData', 'TableData')->name('tableData');
+                Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+
+                Route::get('options', 'Options')->name('options');
+                Route::get('suppliers', 'Suppliers')->name('suppliers');
+            });
     });
