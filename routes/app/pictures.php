16670a8223884f2668297c53e82d6706d80fd231
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:34:17 +0300

    updates routes

--- routes/app/pictures.php
+++ routes/app/pictures.php
@@ -1,13 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Products\Http\Controllers\Pictures\Destroy;
+use LaravelEnso\Products\Http\Controllers\Pictures\Index;
+use LaravelEnso\Products\Http\Controllers\Pictures\Reorder;
+use LaravelEnso\Products\Http\Controllers\Pictures\Store;
 
-Route::namespace('Pictures')
-    ->prefix('pictures')
+Route::prefix('pictures')
     ->as('pictures.')
     ->group(function () {
-        Route::get('{product}/index', 'Index')->name('index');
-        Route::post('{product}/store', 'Store')->name('store');
-        Route::delete('{picture}', 'Destroy')->name('destroy');
-        Route::patch('{picture}/reorder', 'Reorder')->name('reorder');
+        Route::get('{product}/index', Index::class)->name('index');
+        Route::post('{product}/store', Store::class)->name('store');
+        Route::delete('{picture}', Destroy::class)->name('destroy');
+        Route::patch('{picture}/reorder', Reorder::class)->name('reorder');
     });
--- routes/app/products.php
+++ routes/app/products.php
@@ -1,16 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Products\Http\Controllers\Create;
+use LaravelEnso\Products\Http\Controllers\Destroy;
+use LaravelEnso\Products\Http\Controllers\Edit;
+use LaravelEnso\Products\Http\Controllers\ExportExcel;
+use LaravelEnso\Products\Http\Controllers\InitTable;
+use LaravelEnso\Products\Http\Controllers\Options;
+use LaravelEnso\Products\Http\Controllers\Store;
+use LaravelEnso\Products\Http\Controllers\Suppliers;
+use LaravelEnso\Products\Http\Controllers\TableData;
+use LaravelEnso\Products\Http\Controllers\Update;
 
-Route::get('create', 'Create')->name('create');
-Route::post('', 'Store')->name('store');
-Route::get('{product}/edit', 'Edit')->name('edit');
-Route::patch('{product}', 'Update')->name('update');
-Route::delete('{product}', 'Destroy')->name('destroy');
+Route::get('create', Create::class)->name('create');
+Route::post('', Store::class)->name('store');
+Route::get('{product}/edit', Edit::class)->name('edit');
+Route::patch('{product}', Update::class)->name('update');
+Route::delete('{product}', Destroy::class)->name('destroy');
 
-Route::get('initTable', 'InitTable')->name('initTable');
-Route::get('tableData', 'TableData')->name('tableData');
-Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+Route::get('initTable', InitTable::class)->name('initTable');
+Route::get('tableData', TableData::class)->name('tableData');
+Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
 
-Route::get('options', 'Options')->name('options');
-Route::get('suppliers', 'Suppliers')->name('suppliers');
+Route::get('options', Options::class)->name('options');
+Route::get('suppliers', Suppliers::class)->name('suppliers');
